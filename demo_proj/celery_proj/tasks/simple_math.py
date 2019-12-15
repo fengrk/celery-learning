ef3c45e3664fdf420a54dbc754bae493c29bce7e
frkht <frkhit@gmail.com>
2019-12-15 23:13:03 +0800

    celery: complex project, show Q-A, Q-B in flower

--- demo_proj/celery_proj/tasks/simple_math.py
+++ demo_proj/celery_proj/tasks/simple_math.py
@@ -1,11 +1,14 @@
 # -*- coding: utf-8 -*-
 
+import time
+
 from celery_proj.app import celery_app
 from libs.demo_utils import demo_add
 
 
 @celery_app.task
 def demo_sum(a: float, b: float, c: float) -> float:
+    time.sleep(5)
     _result = demo_add(demo_add(a, b), c)
     print("demo_sum: a {}, b {}, c {} => {}".format(a, b, c, _result))
     return _result
@@ -13,6 +16,7 @@ def demo_sum(a: float, b: float, c: float) -> float:
 
 @celery_app.task
 def demo_func(a: float, b: float) -> float:
+    time.sleep(1)
     _result = a * 2 + b * b
     print("demo_func: a {}, b {} => {}".format(a, b, _result))
     return _result
