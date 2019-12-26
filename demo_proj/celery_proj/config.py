e01d00192305318264377634a124e7b32ed3343a
frkht <frkhit@gmail.com>
2019-12-26 17:21:11 +0800

    ack late

--- demo_proj/celery_proj/config.py
+++ demo_proj/celery_proj/config.py
@@ -1,7 +1,11 @@
+from kombu import Queue
+
 # 消息
 CELERY_TIMEZONE = 'Asia/Shanghai'
 CELERY_MESSAGE_COMPRESSION = "gzip"
 CELERY_TASK_SERIALIZER = "json"
+CELERY_ACKS_LATE = True
+CELERYD_PREFETCH_MULTIPLIER = 1
 
 CELERY_RESULT_BACKEND = BROKER_URL = "redis://redis-stream:6379/0"
 
@@ -9,15 +13,18 @@ CELERY_RESULT_BACKEND = BROKER_URL = "redis://redis-stream:6379/0"
 QUEUE_A = "Q-A"
 QUEUE_B = "Q-B"
 
-CELERY_QUEUES = {
-    QUEUE_A: {"routing_key": QUEUE_A, "exchange": QUEUE_A},
-    QUEUE_B: {"routing_key": QUEUE_B, "exchange": QUEUE_B},
-}
+# default queue
+CELERY_TASK_DEFAULT_QUEUE = QUEUE_A
+CELERY_TASK_CREATE_MISSING_QUEUES = True
 
-# 这个比较重要，和上面的任务队列必须匹配使用，用来把任务队列配到相应函数上
+CELERY_QUEUES = (
+    Queue(name=QUEUE_A, exchange=QUEUE_A, routing_key=QUEUE_A),
+    Queue(name=QUEUE_B, exchange=QUEUE_B, routing_key=QUEUE_B),
+)
+
 CELERY_ROUTES = {
-    'celery_proj.tasks.simple_math.demo_sum': {"routing_key": QUEUE_A, "queue": QUEUE_A},
-    'celery_proj.tasks.simple_math.demo_func': {"routing_key": QUEUE_B, "queue": QUEUE_B},
+    'celery_proj.tasks.simple_math.demo_sum': {"queue": QUEUE_A},
+    'celery_proj.tasks.simple_math.demo_func': {"queue": QUEUE_B},
 }
 
 # include all task
